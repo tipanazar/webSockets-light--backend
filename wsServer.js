2c8b09b4fd4a0563c426a6ca33eb8a7131734a4f
ololosh-tralevich <karpeko76@gmail.com>
2022-08-09 20:47:22 +0100

    upd

---
+++ wsServer.js
@@ -0,0 +1,43 @@
+// const express = require("express");
+// const http = require("http");
+// const path = require("path");
+const ws = new require("ws");
+
+// const app = express();
+let users = [];
+
+// app.use(express.static(path.join(__dirname, "./public")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// const httpServer = http.createServer(app);
+const wsServer = new ws.Server({ port: 5000 });
+// const wsServer = new ws.Server({ server: httpServer });
+
+wsServer.on("connection", (newUser) => {
+  users.push(newUser);
+  setTimeout(() => {
+    newUser.send("Вы в чате");
+  }, 1000);
+
+  newUser.on("message", (data) => {
+    const parsedData = JSON.parse(data);
+    const stringedData = JSON.stringify(parsedData);
+    users.forEach((user) => {
+      if (user !== newUser) {
+        user.send(stringedData);
+      }
+    });
+  });
+
+  users.forEach((user) => {
+    if (user !== newUser) {
+      user.send("У нас новый участник!!!");
+    }
+  });
+});
+
+// httpServer.listen(5000, () => {
+//   console.log("Server started. Port: ", 5000);
+// });

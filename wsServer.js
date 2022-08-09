f56f3a1430c5299fc1d08e9d71c58931f71e4497
ololosh-tralevich <karpeko76@gmail.com>
2022-08-09 22:46:03 +0100

    upd

--- wsServer.js
+++ wsServer.js
@@ -1,35 +1,35 @@
-const express = require("express");
 const ws = new require("ws");
 
 const PORT = process.env.PORT || 5000;
 
-// const server = express()
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wsServer = new ws.Server({port: PORT});
+const wsServer = new ws.Server({ port: PORT });
 
 let users = [];
-wsServer.on("connection", (newUser) => {
-  console.log("Connected to WS server");
-  users.push(newUser);
+wsServer
+  .on("connection", (newUser) => {
+    console.log(`Connected to WS server, port: ${PORT}`);
+    users.push(newUser);
 
-  setTimeout(() => {
-    newUser.send("Вы в чате");
-  }, 1000);
+    setTimeout(() => {
+      newUser.send("Вы в чате");
+    }, 1000);
 
-  newUser.on("message", (data) => {
-    const parsedData = JSON.parse(data);
-    const stringedData = JSON.stringify(parsedData);
+    newUser.on("message", (data) => {
+      const parsedData = JSON.parse(data);
+      const stringedData = JSON.stringify(parsedData);
+      users.forEach((user) => {
+        if (user !== newUser) {
+          user.send(stringedData);
+        }
+      });
+    });
+
     users.forEach((user) => {
       if (user !== newUser) {
-        user.send(stringedData);
+        user.send("У нас новый участник!!!");
       }
     });
+  })
+  .listen(() => {
+    console.log("condasd");
   });
-
-  users.forEach((user) => {
-    if (user !== newUser) {
-      user.send("У нас новый участник!!!");
-    }
-  });
-});

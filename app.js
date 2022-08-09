46b84ede3e7246c7fd2b8b3b49537a75b909d561
ololosh-tralevich <karpeko76@gmail.com>
2022-08-09 20:10:02 +0100

    upd

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-const ws = new require("ws");
+const app = express();
+app.use(express.static(path.join(__dirname, "./public")));
+app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "index.html")) });
 
-const wsServer = new ws.Server({ port: 3000 });
-
-const users = [];
-
-wsServer.on("connection", (newUser) => {
-  users.push(newUser);
-  setTimeout(() => {
-    newUser.send("Вы в чате");
-  }, 1000);
-
-  newUser.on("message", (data) => {
-    const parsedData = JSON.parse(data);
-    const stringedData = JSON.stringify(parsedData);
-    users.forEach((user) => {
-      if (user !== newUser) {
-        user.send(stringedData);
-      }
+const httpServer = http.createServer(app);
+const wss = new ws.Server({ server: httpServer });
+wss.on("connection",
+    (ws) =>
+    {
+        console.log("Client connected");
+        ws.onmessage =
+            (event) =>
+            {
+                const msg = JSON.parse(event.data);
+                console.log(msg.x + ", " + msg.y);
+                // Send an answer
+                const resp = {
+                    x: msg.x,
+                    y: msg.y
+                }
+                ws.send(JSON.stringify(resp));
+            }
     });
-  });
 
-  users.forEach((user) => {
-    if (user !== newUser) {
-      user.send("У нас новый участник!!!");
-    }
-  });
-});
+const port = process.env.PORT || 3000;
+httpServer.listen(port, () => { console.log("Server started. Port: ", port); });
